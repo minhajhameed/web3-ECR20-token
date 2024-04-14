5643cabf8787a9c9d0d9dbdb8777d2ae431a2124
minhajhameed <minhajham@yahoo.com>
2024-04-14 12:02:00 +0400

    add: contract constant

---
+++ Context/constants.js
@@ -0,0 +1,14 @@
+//CryptoFox: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+// TokenSale: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+
+
+import CryptoFox from './CryptoFox.json';
+import TokenSale from './TokenSale.json';
+
+
+export const TOKEN_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const TOKEN_ABI = CryptoFox.abi;
+
+export const TOKEN_SALE_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const TOKEN_SALE_ABI = TokenSale.abi;
